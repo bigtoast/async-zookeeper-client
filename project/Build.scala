2d5c711f628d4407c74cd53198a2009ed3b1366b
Dragisa Krsmanovic <dragisak@ticketfly.com>
2014-02-26 17:34:30 -0800

    Cosmetic cleanup

--- project/Build.scala
+++ project/Build.scala
@@ -7,11 +7,17 @@ object AsyncZkClient extends Build {
 
   val VERSION = "0.3.0"
 
-  val dependencies =
-    "com.typesafe.akka"    %  "akka-actor" % "2.0.5" ::
-    "org.apache.zookeeper" %  "zookeeper"  % "3.4.5" ::
-    "org.scalatest"        %% "scalatest"  % "1.9.1" % "test" :: 
-    "com.github.bigtoast"  %% "rokprox"    % "0.2.0" % "test" :: Nil
+  val dependencies =  Seq(
+    "com.typesafe.akka"    %  "akka-actor"         % "2.0.5",
+    "org.apache.zookeeper" %  "zookeeper"          % "3.4.5" excludeAll (
+                                                        ExclusionRule(organization = "log4j"),
+                                                        ExclusionRule(organization = "org.slf4j")
+                                                     ),
+    "org.slf4j"            %  "log4j-over-slf4j"   % "1.7.5",
+    "org.scalatest"        %% "scalatest"          % "1.9.1" % "test",
+    "com.github.bigtoast"  %% "rokprox"            % "0.2.0" % "test",
+    "org.slf4j"            %  "slf4j-simple"       % "1.7.5" % "test"
+  )
 
   val publishDocs = TaskKey[Unit]("publish-docs")
 
@@ -21,14 +27,6 @@ object AsyncZkClient extends Build {
       name         := "async-zk-client",
       version      := VERSION,
       scalaVersion := "2.9.2",
-
-      ivyXML :=
-        <dependencies>
-          <exclude org="com.sun.jmx" module="jmxri" />
-          <exclude org="com.sun.jdmk" module="jmxtools" />
-          <exclude org="javax.jms" module="jms" />
-          <exclude org="thrift" module="libthrift" />
-        </dependencies>,
 
       publishTo := Some(Resolver.file("bigtoast.github.com", file(Path.userHome + "/Projects/BigToast/bigtoast.github.com/repo"))),
 
@@ -43,5 +41,5 @@ object AsyncZkClient extends Build {
 
       libraryDependencies ++= dependencies
 
-    ))
+    )).settings(net.virtualvoid.sbt.graph.Plugin.graphSettings: _*)
 }
